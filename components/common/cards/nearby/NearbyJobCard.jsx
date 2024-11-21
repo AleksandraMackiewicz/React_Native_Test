44c2428b0d314e08a266986c33e20f31ca97adac
= <=>
2024-11-22 00:31:00 +0100

    fiexed <text>

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -8,7 +8,7 @@ const NearbyJobCard = ({ job, handleNavigate}) => {
   return (
     <TouchableOpacity
     style={styles.container}
-    onPress={() => handleNavigate}
+    onPress={handleNavigate}
     >
       <TouchableOpacity style={styles.logoContainer}>
         <Image
@@ -33,8 +33,7 @@ const NearbyJobCard = ({ job, handleNavigate}) => {
 
 
     </TouchableOpacity>
- 
-  )
+   )
 }
 
 export default NearbyJobCard;
